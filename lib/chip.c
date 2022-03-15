dbf5d2ad0dfa5b2c96bfab2ed1189128b27ef9d9
Bartosz Golaszewski <brgl@bgdev.pl>
2022-03-15 13:09:58 +0100

    WIP: lib: remove redundant comments
    The file names are indicative of the role of the code they contain.
    Drop the unnecessary comments.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>

--- lib/chip.c
+++ lib/chip.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // SPDX-FileCopyrightText: 2021 Bartosz Golaszewski <brgl@bgdev.pl>
 
-/* Line attribute data structure and functions. */
-
 #include <errno.h>
 #include <fcntl.h>
 #include <gpiod.h>
--- lib/info-event.c
+++ lib/info-event.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // SPDX-FileCopyrightText: 2021 Bartosz Golaszewski <brgl@bgdev.pl>
 
-/* Line status watch. */
-
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
