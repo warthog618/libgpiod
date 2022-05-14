635ab91fb2784611a252ef522b2da5eab636ed34
Bartosz Golaszewski <brgl@bgdev.pl>
2022-05-14 15:42:31 +0200

    WIP: tests: require linux v5.17.4
    There was a bug in gpio-sim module that was only fixed in v5.17.4.
    
    Signed-off-by: Bartosz Golaszewski <brgl@bgdev.pl>

--- tests/gpiosim/gpiosim.c
+++ tests/gpiosim/gpiosim.c
@@ -22,7 +22,7 @@
 
 #define GPIOSIM_API		__attribute__((visibility("default")))
 #define ARRAY_SIZE(x)		(sizeof(x) / sizeof(*(x)))
-#define MIN_KERNEL_VERSION	KERNEL_VERSION(5, 17, 0)
+#define MIN_KERNEL_VERSION	KERNEL_VERSION(5, 17, 4)
 
 static pthread_mutex_t id_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_once_t id_init_once = PTHREAD_ONCE_INIT;
