4905c33ea4c0792cd56051066d13b60b5a190875
Kent Gibson <warthog618@gmail.com>
2022-06-13 15:51:04 +0800

    gpioinfo: rework

--- tools/gpioinfo.c
+++ tools/gpioinfo.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // SPDX-FileCopyrightText: 2017-2021 Bartosz Golaszewski <bartekgola@gmail.com>
 
-#include <dirent.h>
 #include <errno.h>
 #include <getopt.h>
 #include <gpiod.h>
@@ -12,44 +11,97 @@
 
 #include "tools-common.h"
 
+static int by_name = 0;
+
 static const struct option longopts[] = {
-	{ "help",	no_argument,	NULL,	'h' },
-	{ "version",	no_argument,	NULL,	'v' },
+	{ "chip",	required_argument,	NULL,	'c' },
+	{ "by-name",	no_argument,		&by_name,	1 },
+	{ "help",	no_argument,		NULL,	'h' },
+	{ "strict",	no_argument,		NULL,	's' },
+	{ "version",	no_argument,		NULL,	'v' },
 	{ GETOPT_NULL_LONGOPT },
 };
 
-static const char *const shortopts = "+hv";
+static const char *const shortopts = "+c:shv";
 
 static void print_help(void)
 {
-	printf("Usage: %s [OPTIONS] <gpiochip1> ...\n", get_progname());
+	printf("Usage: %s [OPTIONS] [line] ...\n", get_progname());
 	printf("\n");
-	printf("Print information about all lines of the specified GPIO chip(s) (or all gpiochips if none are specified).\n");
+	printf("Print information about the specified line(s).\n");
+	printf("\n");
+	printf("Lines are specified by name, or optionally by offset if the chip option\n");
+	printf("is provided.\n");
+	printf("\n");
+	printf("If no lines are specified than all lines are displayed.\n");
 	printf("\n");
 	printf("Options:\n");
-	printf("  -h, --help:\t\tdisplay this message and exit\n");
-	printf("  -v, --version:\tdisplay the version and exit\n");
+	print_chip_help();
+	printf("      --by-name\t\ttreat lines as names even if they would parse as an offset\n");
+	printf("  -s, --strict\t\tcheck all lines - don't assume line names are unique\n");
+	printf("  -h, --help\t\tdisplay this message and exit\n");
+	printf("  -v, --version\t\tdisplay the version and exit\n");
 }
 
-static void list_lines(struct gpiod_chip *chip)
+static bool filter_line(struct gpiod_line_info *info, struct line_resolver *resolver) {
+	int i;
+	struct resolved_line *line;
+	bool skip = true;
+	const char *name;
+
+	if (!resolver)
+		return false;
+
+	for (i = 0; i < resolver->num_lines; i++) {
+		line = &resolver->lines[i];
+		name = gpiod_line_info_get_name(info);
+		if (((line->offset == (int)gpiod_line_info_get_offset(info)) ||
+		     (name && strcmp(line->id, name) == 0)) &&
+		    (resolver->strict || !line->found)) {
+			skip = false;
+			line->found = true;
+			resolver->num_found++;
+		}
+	}
+	return skip;
+}
+
+static bool filter_done(struct line_resolver *resolver) {
+	return (resolver && !resolver->strict && resolver->num_found >= resolver->num_lines);
+}
+
+static void list_lines(struct gpiod_chip *chip, struct line_resolver *resolver)
 {
 	struct gpiod_chip_info *chip_info;
 	struct gpiod_line_info *info;
-	size_t offset, num_lines;
+	int offset, num_lines;
 
 	chip_info = gpiod_chip_get_info(chip);
 	if (!chip_info)
-		die_perror("unable to retrieve the chip info from chip");
+		die_perror("unable to retrieve the chip info from chip %s",
+			   gpiod_chip_get_path(chip));
 
 	num_lines = gpiod_chip_info_get_num_lines(chip_info);
-	printf("%s - %zu lines:\n",
-	       gpiod_chip_info_get_name(chip_info), num_lines);
 
-	for (offset = 0; offset < num_lines; offset++) {
+	for (offset = 0; ((offset < num_lines) && !filter_done(resolver)); offset++) {
 		info = gpiod_chip_get_line_info(chip, offset);
 		if (!info)
-			die_perror("unable to retrieve the line info from chip");
+			die_perror("unable to retrieve the line info from chip %s",
+				   gpiod_chip_get_path(chip));
+
+		if (filter_line(info, resolver))
+			continue;
+
+		if (resolver && resolver->num_lines) {
+			printf("%s %u", gpiod_chip_info_get_name(chip_info), offset);
+		} else {
+			if (offset == 0)
+				printf("%s - %u lines:\n",
+				       gpiod_chip_info_get_name(chip_info), num_lines);
+			printf("\tline %3u:", offset);
+		}
 		print_line_info(info);
+		printf("\n");
 		gpiod_line_info_free(info);
 	}
 	gpiod_chip_info_free(chip_info);
@@ -57,9 +109,12 @@ static void list_lines(struct gpiod_chip *chip)
 
 int main(int argc, char **argv)
 {
-	int num_chips, i, optc, opti;
+	int optc, opti, num_chips, i, ret = EXIT_SUCCESS;
 	struct gpiod_chip *chip;
-	struct dirent **entries;
+	char *chip_id = NULL;
+	bool strict = false;
+	char ** paths;
+	struct line_resolver *resolver = NULL;
 
 	for (;;) {
 		optc = getopt_long(argc, argv, shortopts, longopts, &opti);
@@ -67,6 +122,12 @@ int main(int argc, char **argv)
 			break;
 
 		switch (optc) {
+		case 'c':
+			chip_id = optarg;
+			break;
+		case 's':
+			strict = true;
+			break;
 		case 'h':
 			print_help();
 			return EXIT_SUCCESS;
@@ -75,6 +136,8 @@ int main(int argc, char **argv)
 			return EXIT_SUCCESS;
 		case '?':
 			die("try %s --help", get_progname());
+		case 0:
+			break;
 		default:
 			abort();
 		}
@@ -83,35 +146,52 @@ int main(int argc, char **argv)
 	argc -= optind;
 	argv += optind;
 
-	if (argc == 0) {
-		num_chips = scandir("/dev/", &entries,
-				    chip_dir_filter, alphasort);
-		if (num_chips < 0)
-			die_perror("unable to scan /dev");
+	if (chip_id == NULL)
+		by_name = true;
 
-		for (i = 0; i < num_chips; i++) {
-			chip = chip_open_by_name(entries[i]->d_name);
-			if (!chip)
-				die_perror("unable to open %s",
-					   entries[i]->d_name);
+	num_chips = chip_paths(chip_id, &paths);
+	if ((chip_id != NULL)  && (num_chips == 0))
+		die("cannot find a GPIO chip character device corresponding to %s", chip_id);
 
-			list_lines(chip);
-
-			gpiod_chip_close(chip);
-			free(entries[i]);
-		}
-		free(entries);
-	} else {
+	if (argc) {
+		resolver = malloc(sizeof(*resolver) + argc * sizeof(struct resolved_line));
+		if (resolver == NULL)
+			die("out of memory");
+		resolver->num_lines = argc;
+		resolver->num_found = 0;
+		resolver->strict = strict;
 		for (i = 0; i < argc; i++) {
-			chip = chip_open_lookup(argv[i]);
-			if (!chip)
-				die_perror("looking up chip %s", argv[i]);
-
-			list_lines(chip);
-
-			gpiod_chip_close(chip);
+			resolver->lines[i].id = argv[i];
+			resolver->lines[i].found = false;
+			resolver->lines[i].offset = by_name ? -1 : parse_uint(argv[i]);
 		}
 	}
 
-	return EXIT_SUCCESS;
+	for (i = 0; i < num_chips; i++) {
+		chip = gpiod_chip_open(paths[i]);
+		if (!chip) {
+			if ((errno != EACCES) && (chip_id != NULL)) {
+				log_perror("unable to open chip %s", paths[i]);
+				ret = EXIT_FAILURE;
+			}
+			continue;
+		}
+		list_lines(chip, resolver);
+		gpiod_chip_close(chip);
+		free(paths[i]);
+	}
+	free(paths);
+
+	if (resolver) {
+		for (i = 0; i < resolver->num_lines; i++) {
+			if (!resolver->lines[i].found) {
+				fprintf(stderr, "cannot find line %s\n", resolver->lines[i].id);
+				ret = EXIT_FAILURE;
+			}
+		}
+		if (resolver->num_lines != resolver->num_found)
+			ret = EXIT_FAILURE;
+		free(resolver);
+	}
+	return ret;
 }
